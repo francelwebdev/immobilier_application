dee0a0f14b6a71533db6823f1ee4983483ca0c69
Francel WebDev <francel.webdev@gmail.com>
2018-03-08 23:24:48 +0100

    j'ai mis en place quelque validation dans le model property

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+# Le code ci-dessous permet de rediriger l'utilisateur vers un autre page après connexion
+  def after_sign_in_path_for(resource)
+	  new_property_path(current_user) #your path
+	end
+# ok
+
   before_action :authenticate_user!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180304225431) do
+ActiveRecord::Schema.define(version: 20180308205613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,12 +70,6 @@ ActiveRecord::Schema.define(version: 20180304225431) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "number"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "specialties", force: :cascade do |t|
-    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
