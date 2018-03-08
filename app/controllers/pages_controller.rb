1e38769ee65f368d224528de7ebaffc056b82c5b
Francel <francel.webdev@gmail.com>
2018-03-08 23:50:02 +0100

    ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,8 @@
 class PagesController < ApplicationController
-
     skip_before_action :authenticate_user!, only: [:home]
 
     def home
         @ad_type = AdType.all
         @property_type = PropertyType.all
     end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'pages/home'
+
     root to: "pages#home", as: "accueil"
 
     devise_for :users
