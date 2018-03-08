11fd80a954ad67bd9cbd7aae615bd12ea17cd3db
Francel <francel.webdev@gmail.com>
2018-03-08 23:45:24 +0100

    supression du controller welcome dans la master

--- app/controllers/welcome_controller.rb
+++
@@ -1,8 +0,0 @@
-class WelcomeController < ApplicationController
-	skip_before_action :authenticate_user!, only: [:index]
-
-  def index
-    @ad_type = AdType.all
-    @property_type = PropertyType.all
-  end
-end
