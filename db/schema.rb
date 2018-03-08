7f57e0ac5f7b0cb03aaf5aabcfa721778915e03a
Francel WebDev <francel.webdev@gmail.com>
2018-03-08 22:25:32 +0100

    ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180309033802) do
+ActiveRecord::Schema.define(version: 20180309041005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,9 +91,11 @@ ActiveRecord::Schema.define(version: 20180309033802) do
     t.string "last_name"
     t.bigint "phone_number"
     t.string "profile_photo"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["phone_number"], name: "index_users_on_phone_number", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "properties", "ad_types"
