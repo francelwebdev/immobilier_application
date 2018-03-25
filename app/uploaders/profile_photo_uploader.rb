98bc1f0937f210f7d65406e42da09ac9db825f6a
Francel WebDev <francel.webdev@gmail.com>
2018-03-25 18:02:09 +0200

    ok

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -1,13 +1,19 @@
 class ProfilePhotoUploader < CarrierWave::Uploader::Base
 
-  # include Cloudinary::CarrierWave
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
    include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-   storage :file
+
+  if Rails.env.poduction?
+    include Cloudinary::CarrierWave
+  else
+    storage :file
+  end
+  
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
