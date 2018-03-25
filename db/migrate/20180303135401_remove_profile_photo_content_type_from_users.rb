0c60c01a81b045bab4ced165397df9e6f0e73e88
Francel WebDev <francel.webdev@gmail.com>
2018-03-25 19:12:57 +0200

    ok

--- db/migrate/20180303135401_remove_profile_photo_content_type_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveProfilePhotoContentTypeFromUsers < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :users, :profile_photo_content_type, :string
-  end
-end
--- db/migrate/20180325170136_add_profile_photo_content_type_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddProfilePhotoContentTypeToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :profile_photo_content_type, :string
-  end
-end
