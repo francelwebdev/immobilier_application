2da7449c1815af5d2fa5a080a69e472419cc71b0
Francel <francel.webdev@gmail.com>
2018-05-06 13:09:47 +0200

    ok ok

---
+++ app/assets/javascripts/typed_init.js
@@ -0,0 +1,10 @@
+$(function(){
+    $(".element").typed({
+        strings: ["une chambre", "une maison", "un appartement", "un bureau", "une boutique", "un terrain", "une parcelle", "un immeuble"],
+        typeSpeed: 50,
+        backSpeed: 25,
+        backDelay: 500,
+        startDelay: 1000,
+        loop: true,
+    });
+});
