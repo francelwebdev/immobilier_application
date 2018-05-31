6e57fefc1bd06fa88b37631d0a8c5c426a386f01
Francel <francel.webdev@gmail.com>
2018-05-31 12:17:54 +0200

    ok

--- db/migrate/20180518195837_add_profile_column_to_profile.rb
+++
@@ -1,9 +0,0 @@
-class AddProfileColumnToProfile < ActiveRecord::Migration[5.2]
-  def change
-    add_column :profiles, :first_name, :string
-    add_column :profiles, :last_name, :string
-    add_column :profiles, :phone_number, :integer, limit: 8
-    add_column :profiles, :address, :string
-    add_column :profiles, :about_me, :text
-  end
-end
--- db/migrate/20180518205153_add_gender_to_profiles.rb
+++
@@ -1,5 +0,0 @@
-class AddGenderToProfiles < ActiveRecord::Migration[5.2]
-  def change
-    add_column :profiles, :gender, :string
-  end
-end
