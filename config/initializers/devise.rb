f303bb3fcb011dff7a97e7b5c8a4cab86b5c1ffd
Francel <francel.webdev@gmail.com>
2018-06-05 12:48:17 +0200

    ok

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -255,7 +255,7 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
   # Mon code
-  config.omniauth :facebook, "220002002137902", "5337b26211786fa2c994e1d85da14a96", , token_params: { parse: :json }, scope: 'email', info_fields: 'email, first_name, last_name, image', image_size: 'square', secure_image_url: true
+  config.omniauth :facebook, "220002002137902", "5337b26211786fa2c994e1d85da14a96", token_params: { parse: :json }, scope: 'email', info_fields: 'email, first_name, last_name, image', image_size: 'square', secure_image_url: true
 # Mon code
 
   # ==> Warden configuration
