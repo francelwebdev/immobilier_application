bc8947ddbd6adbac709feb3e0d0a2d60bdbf54e6
Francel <francel.webdev@gmail.com>
2018-07-02 19:29:34 +0200

    ok

--- app/models/property.rb
+++ app/models/property.rb
@@ -5,7 +5,7 @@ class Property < ApplicationRecord
     ROOM = ["1 pièce", "2 pièces", "3 pièces", "4 pièces", "5 pièces", "plus de 5 pièces"]
     FEATURE = ["Electricité", "Eau", "WC", "Internet"]
     ETAGE = ["Rez-de-chaussée", "1er étage", "2ème étage", "3ème étage", "4ème étage", "5ème étage"]
-    AD_TYPE = ["Location"]
+    AD_TYPE = ["A louer", "A vendre"]
     PROPERTY_TYPE = ["Appartement", "Maison", "Bureau", "Boutique", "Villa", "Chambre", "Entrée couché", "Magasin"]
     AVAILABLE = { Oui: :true, Nom: :false }
 
