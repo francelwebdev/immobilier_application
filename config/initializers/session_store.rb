ba3b796a5e565d208fa916a022a4d3aee45f6d75
Francel <francel.webdev@gmail.com>
2018-07-09 19:30:52 +0200

    ok

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,1 @@
 # Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :active_record_store, :key => '_immobilier_application_session'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_05_190111) do
+ActiveRecord::Schema.define(version: 2018_07_09_172954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -124,15 +124,6 @@ ActiveRecord::Schema.define(version: 2018_07_05_190111) do
     t.index ["property_type"], name: "index_properties_on_property_type"
     t.index ["slug"], name: "index_properties_on_slug", unique: true
     t.index ["user_id"], name: "index_properties_on_user_id"
-  end
-
-  create_table "sessions", force: :cascade do |t|
-    t.string "session_id", null: false
-    t.text "data"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
-    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
