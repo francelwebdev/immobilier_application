27dd472123194cc599e9f74c49be1a62bd252479
Francel AMAMOO <francel.webdev@gmail.com>
2018-08-09 08:41:48 +0100

    ok

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -259,9 +259,9 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
 
-  config.omniauth :facebook, Rails.application.secrets.fb_app_id, Rails.application.secrets.fb_app_secret, scope: 'email,first_name,last_name,picture', display: "popup", client_options: {
-      site: 'https://graph.facebook.com/v3.0',
-      authorize_url: "https://www.facebook.com/v3.0/dialog/oauth"
+  config.omniauth :facebook, Rails.application.secrets.fb_app_id, Rails.application.secrets.fb_app_secret, scope: 'email', info_fields: "email,first_name,last_name,picture", display: "popup", client_options: {
+      site: 'https://graph.facebook.com/v3.1',
+      authorize_url: "https://www.facebook.com/v3.1/dialog/oauth"
     }
 
 
